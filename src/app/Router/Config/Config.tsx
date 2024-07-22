1bed8f5e194c54119bc4dcef04b3ee8914be8080
Arsen0312 <greenchay16@gmail.com>
2024-07-22 19:09:37 +0600

    добавил скилет для карточек в интерфейсе

--- src/app/Router/Config/Config.tsx
+++ src/app/Router/Config/Config.tsx
@@ -3,12 +3,14 @@ import HomePage from "../../pages/home-page/home-page.tsx";
 import PlayGame from "../../pages/play-game/play-game.tsx";
 import NewGame from "../../pages/new-game/new-game.tsx";
 import Kvesty from "../../pages/kwesty/Kvesty.tsx";
+import {LeaderBoard} from "../../pages/LeaderBoard";
 
 enum AppRoutes {
     HOMEPAGE = "homePage",
     PLAYGAME = "inGame",
     NEWGAME = "newGame",
     KVESTY = "kvesty",
+    LEADERBOARD = "leaderBoard",
 
     NOT_FOUND = "not_found",
 }
@@ -20,6 +22,7 @@ export const RoutePaths: Record<AppRoutes, string> = {
     [AppRoutes.PLAYGAME]: "/inGame",
     [AppRoutes.NEWGAME]: "/newGame",
     [AppRoutes.KVESTY]: "/kvesty",
+    [AppRoutes.LEADERBOARD]: "/leaderBoard",
     //last
     [AppRoutes.NOT_FOUND]: "*"
 };
@@ -41,6 +44,10 @@ export const routerConfig: Record<AppRoutes, AppRouteProps> = {
     [AppRoutes.KVESTY]: {
         path: RoutePaths.kvesty,
         element: <Kvesty/>
+    },
+    [AppRoutes.LEADERBOARD]: {
+        path: RoutePaths.leaderBoard,
+        element: <LeaderBoard/>
     },
     [AppRoutes.NOT_FOUND]: {
         path: RoutePaths.not_found,
--- src/app/pages/LeaderBoard/ui/LiderBoard.tsx
+++ src/app/pages/LeaderBoard/ui/LiderBoard.tsx
@@ -1,10 +1,13 @@
 import cls from "./LeaderBoard.module.scss"
+import Header from "../../Widgets/Header/ui/Header.tsx";
+import SkeletonCurd from "../../../components/SkeletonCurd/ui/SkeletonCurd.tsx";
 
 const LeaderBoard = () => {
 
     return (
         <div className={cls.main}>
-
+            <Header text={"Лидерборд"}/>
+            <SkeletonCurd></SkeletonCurd>
         </div>
     );
 };
