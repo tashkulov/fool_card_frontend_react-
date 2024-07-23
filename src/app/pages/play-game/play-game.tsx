987ab8550a3ffc0fddd17c00d15c8ebf711bafa9
tashkulov <nurislamtashkulov168@gmail.com>
2024-07-23 14:54:47 +0600

    play-game added

--- src/app/pages/play-game/play-game.tsx
+++ src/app/pages/play-game/play-game.tsx
@@ -1,44 +1,82 @@
 import './play-game.css';
-import icon from '../img/kvest2.svg';
-import card1 from '../../../assets/img/card1.svg';
-import card2 from '../../../assets/img/card2.svg';
-import card3 from '../../../assets/img/card3.svg';
-import Ramki from './images/Ramki.svg';
-// import izum from './images/Izumrud.svg';
-// import paint1 from './images/paint1.svg';
-// import paint2 from './images/paint2.svg';
-import leftarea from './images/Left Safe Area.svg';
-import rightarea from './images/Right Safe Area.svg';
+
+// import icon from '../../img/kvest2.svg';
+import card1 from '../img/card1.svg';
+import card2 from '../img/card2.svg';
+import card3 from '../img/card3.svg';
+import GamePlay from "../img/Gameplay_Avatar.svg"
+import coins from "../img/coins.svg"
+import arrow from "../img/Arrow1.svg"
 
 const PlayGame = () => {
+
     return (
-        <div className="container">
-            <div className="header">
-                <div className="stafka">
-                    <img src={icon} alt="icon" width={24} height={24} />
-                    <h2>4K</h2>
+        <>
+            <div className="wrapper">
+               
+                <div className="plays">
+                    <section className="play-header">
+                        <div className="container">
+                            <div className="play-header-wrapper">
+                                <div className="play-header-block">
+                                    <a className="play-header-back block-obvodka">
+                                        <img src={arrow} alt="" />
+                                    </a>
+                                    <div className="play-header-coin">
+                                        <img src={coins} alt="" />
+                                        <p>4K</p>
+                                    </div>
+                                </div>
+                                <div className="play-header-rejim block-obvodka">
+                                    <img src={card1} alt="" />
+                                    <img src={card2} alt="" />
+                                    <img src={card3} alt="" />
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <div className="play-header-polosa "></div>
+                    <div className="play-header-polosa "></div>
                 </div>
-                <div className="game-mode">
-                    <img src={card1} alt="card1" width={17} height={22} />
-                    <img src={card2} alt="card2" width={17} height={22} />
-                    <img src={card3} alt="card3" width={17} height={22} />
+                <div className="main play-wrapper play-krug">
+                    <div className="main-wrapper-plays">
+                        <div className="container">
+                            <div className="wrapper-plays-header"></div>
+                            <div className="wrapper-plays-game">
+                                <div className="players-blocks ">
+                                    <div className="player-block user-dumaet footer-ava-wp ">
+                                        <img src={GamePlay} alt="" />
+                                    </div>
+                                    <div className="players-flex">
+                                        <div className="player-block footer-ava-wp">
+                                            <img src={GamePlay} alt="" />
+                                        </div>
+                                        <div className="player-block footer-ava-wp">
+                                            <img src={GamePlay} alt="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="play-footer">
+                    <div className="play-footer-ava">
+                        <div className="footer-ava-roga">
+                            <div className="footer-ava-wp">
+                                <img src="../img/Gameplay_Avatar.svg" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="play-footer-wrap">
+                        <div className="play-footer-btn block-obvodka" id="canvas">
+                            <p>Бито</p>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </>
 
-            <div className="illustration">
-                <div className="left-side">
-                    <img src={leftarea} alt="leftarea" width={20} height={715}/>
-
-                </div>
-                {/*<img src={paint1} width={30} height={53} alt="izum"/>*/}
-                {/*<img src={izum} width={75} height={75} alt="izum"/>*/}
-                {/*<img src={paint2} width={52} height={27} alt="izum"/>*/}
-                <img src={Ramki} width={355} height={200} alt="ramki" className={'ramki'}/>
-                <div className="right-side">
-                    <img src={rightarea} alt="rightarea" width={20} height={715}/>
-                </div>
-            </div>
-        </div>
     );
 };
 
