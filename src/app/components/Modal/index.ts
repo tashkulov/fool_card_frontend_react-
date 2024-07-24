91d0997cc07d5137e65bde9d6176630bc4b1bd47
tashkulov <nurislamtashkulov168@gmail.com>
2024-07-24 13:27:03 +0600

    added

--- src/app/components/Modal/index.ts
+++ src/app/components/Modal/index.ts
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
+
 import Modal from "./ui/Modal";
 
 export {
     Modal
 }
-=======
-import Modal from "./ui/Modal";
-
-export {
-    Modal
-}
->>>>>>> 21d851925464cc27aef99cc603aa18814cbf6369
--- src/app/hooks/useOutsideClick/useOutsideClick.ts
+++ src/app/hooks/useOutsideClick/useOutsideClick.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect } from 'react';
 
 const useOutsideClick = (ref: any, callback: () => void) => {
@@ -16,22 +15,3 @@ const useOutsideClick = (ref: any, callback: () => void) => {
 };
 
 export default useOutsideClick
-=======
-import { useEffect } from 'react';
-
-const useOutsideClick = (ref: any, callback: () => void) => {
-    const handleClick = (e: any) => {
-        if (ref.current && !ref.current.contains(e.target)) {
-            callback()
-        }
-    }
-    useEffect(() => {
-        document.addEventListener("mousedown", handleClick)
-        return () => {
-            document.removeEventListener("mousedown", handleClick)
-        }
-    }, []);
-};
-
-export default useOutsideClick
->>>>>>> 21d851925464cc27aef99cc603aa18814cbf6369
