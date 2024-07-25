b2d068345dc33cf6dda5ab24cd944e4b84534e7b
tashkulov <nurislamtashkulov168@gmail.com>
2024-07-25 14:56:25 +0600

    play-game fixed 25jule

--- src/app/pages/play-game/play-game.tsx
+++ src/app/pages/play-game/play-game.tsx
@@ -37,6 +37,7 @@ const PlayGame = () => {
     const [isAnimating, setIsAnimating] = useState(false);
     const cardAnimationContainerRef = useRef<HTMLDivElement | null>(null);
     const handRef = useRef<HTMLDivElement | null>(null);
+    const [, setTableCards] = useState<string[]>([]);
 
     const fetchGameData = async () => {
         try {
@@ -95,20 +96,17 @@ const PlayGame = () => {
         return path;
     };
 
-
     const handleCardClick = async (card: string, e: React.MouseEvent<HTMLImageElement>) => {
         if (!cardAnimationContainerRef.current || !handRef.current) return;
 
-        // Добавляем карту в состояние cardsInBita
-
-        // Клонируем карту для анимации
+        // Клонируйте карту для анимации
         const cardClone = e.currentTarget.cloneNode(true) as HTMLImageElement;
         cardClone.classList.add('bita-card', 'animate');
         document.body.appendChild(cardClone);
 
         setSelectedCard(card);
 
-        // Удаление карты из hand
+        // Уберите карту из руки
         e.currentTarget.style.display = 'none';
 
         setTimeout(() => {
@@ -118,11 +116,34 @@ const PlayGame = () => {
             cardClone.classList.remove('animate');
             cardClone.classList.add('final-position');
 
-            // Восстановление карты в hand
+            // Восстановите карту в руке
             e.currentTarget.style.display = 'block';
         }, 500);
     };
 
+    const handleBitaClick = () => {
+        if (!cardAnimationContainerRef.current) return;
+
+        // Получите все карты на столе
+        const cards = document.querySelectorAll('.table-card img');
+
+        cards.forEach((card) => {
+            // Клонируйте каждую карту для анимации
+            const cardClone = card.cloneNode(true) as HTMLImageElement;
+            cardClone.classList.add('bita-card', 'animate');
+            document.body.appendChild(cardClone);
+
+            // Позиционируйте карту в `bita` контейнере
+            setTimeout(() => {
+                cardAnimationContainerRef.current?.appendChild(cardClone);
+                cardClone.classList.remove('animate');
+                cardClone.classList.add('move-to-bita');
+            }, 500);
+        });
+
+        // Очистите карты на столе
+        setTableCards([]);
+    };
 
     if (loading) return <div>Loading...</div>;
     if (error) return <div>{error}</div>;
@@ -132,157 +153,155 @@ const PlayGame = () => {
     const middle = gameData ? Math.floor(gameData.hand.length / 2) : 0;
 
     return (
-        <>
-            <div className="wrapper">
-                <div className="plays">
-                    <section className="play-header">
-                        <div className="play-header-wrapper">
-                            <div className="play-header-block">
-                                <a className="play-header-back block-obvodka">
-                                    <img src={arrow} alt="Back" />
-                                </a>
-                                <div className="play-header-coin">
-                                    <img src={coins} alt="Coins" />
-                                    <p>{betValue !== null ? `${betValue}` : 'N/A'}</p> {/* Display the bet value */}
-                                </div>
-                            </div>
-                            <div className="play-header-rejim block-obvodka">
-                                <img src={card1} alt="Card 1" />
-                                <img src={card2} alt="Card 2" />
-                                <img src={card3} alt="Card 3" />
+        <div className="wrapper">
+            <div className="plays">
+                <section className="play-header">
+                    <div className="play-header-wrapper">
+                        <div className="play-header-block">
+                            <a className="play-header-back block-obvodka">
+                                <img src={arrow} alt="Back" />
+                            </a>
+                            <div className="play-header-coin">
+                                <img src={coins} alt="Coins" />
+                                <p>{betValue !== null ? `${betValue}` : 'N/A'}</p> {/* Display the bet value */}
                             </div>
                         </div>
-                    </section>
-                    <div className="play-header-polosa"></div>
-                    <div className="play-header-polosa"></div>
-                </div>
-                <div className="main play-wrapper-game play-krug">
-                    <div className="main-wrapper-plays">
-                        <div className="wrapper-plays-header"></div>
-                        <div className="wrapper-plays-game">
-                            <div className="players-blocks">
-                                <div className="player-block user-dumaet footer-ava-wp">
+                        <div className="play-header-rejim block-obvodka">
+                            <img src={card1} alt="Card 1" />
+                            <img src={card2} alt="Card 2" />
+                            <img src={card3} alt="Card 3" />
+                        </div>
+                    </div>
+                </section>
+                <div className="play-header-polosa"></div>
+                <div className="play-header-polosa"></div>
+            </div>
+            <div className="main play-wrapper-game play-krug">
+                <div className="main-wrapper-plays">
+                    <div className="wrapper-plays-header"></div>
+                    <div className="wrapper-plays-game">
+                        <div className="players-blocks">
+                            <div className="player-block user-dumaet footer-ava-wp">
+                                <img src={GamePlay} alt="Gameplay Avatar"/>
+                            </div>
+
+                            <div className="players-flex">
+                                <div className="player-block footer-ava-wp">
                                     <img src={GamePlay} alt="Gameplay Avatar"/>
                                 </div>
-
-                                <div className="players-flex">
-                                    <div className="player-block footer-ava-wp">
-                                        <img src={GamePlay} alt="Gameplay Avatar"/>
-                                    </div>
-                                    <div className="player-block footer-ava-wp">
-                                        <img src={GamePlay} alt="Gameplay Avatar"/>
-                                    </div>
+                                <div className="player-block footer-ava-wp">
+                                    <img src={GamePlay} alt="Gameplay Avatar"/>
                                 </div>
                             </div>
                         </div>
+                    </div>
 
-                        <div className="deck">
-                            <div className="card-container">
-                                {gameData && (
-                                    <img
-                                        key={'trump_card'}
-                                        src={getCardImagePath(gameData.trump_card)}
-                                        alt={"card"}
-                                        width={64}
-                                        height={90}
-                                        className="trump-card"
-                                    />
-                                )}
+                    <div className="deck">
+                        <div className="card-container">
+                            {gameData && (
                                 <img
-                                    key={'back_card'}
-                                    src={back_card}
+                                    key={'trump_card'}
+                                    src={getCardImagePath(gameData.trump_card)}
                                     alt={"card"}
                                     width={64}
                                     height={90}
-                                    className="back-card"
-                                />
-                            </div>
-                        </div>
-
-                        <div className="bita">
-                            <div className="card-container">
-                                <img
-                                    key={'bita1'}
-                                    src={back_card}
-                                    alt={"back_card"}
-                                    width={64}
-                                    height={90}
-                                    className="back_card1"
-                                />
-                                <img
-                                    key={'bita2'}
-                                    src={back_card}
-                                    alt={"back_card"}
-                                    width={64}
-                                    height={90}
-                                    className="back_card2"
-                                />
-                                <img
-                                    key={'bita3'}
-                                    src={back_card}
-                                    alt={"back_card"}
-                                    width={64}
-                                    height={90}
-                                    className="back_card3"
-                                />
-                            </div>
-                        </div>
-
-                        <div className="bita-container" ref={cardAnimationContainerRef}>
-                            {selectedCard && (
-                                <img
-                                    src={getCardImagePath(selectedCard)}
-                                    alt={selectedCard}
-                                    className={`bita-card ${isAnimating ? 'animate' : ''}`}
-                                    onAnimationEnd={() => {
-                                        setIsAnimating(false);
-                                        setSelectedCard(null);
-                                    }}
+                                    className="trump-card"
                                 />
                             )}
+                            <img
+                                key={'back_card_in_deck'}
+                                src={back_card}
+                                alt={"back_card_in_deck"}
+                                width={64}
+                                height={90}
+                                className="back-card-in-deck"
+                            />
                         </div>
+                    </div>
 
-                        <div className="hand" ref={handRef}>
-                            {gameData && gameData.hand.map((card: string, index: number) => {
-                                const rotation = (index - middle) * angle;
-                                const position = (index - middle) * offset;
-
-                                return (
-                                    <img
-                                        key={card}
-                                        src={getCardImagePath(card)}
-                                        alt={card}
-                                        style={{
-                                            left: `calc(50% + ${position}px)`,
-                                            transform: `rotate(${rotation}deg)`,
-                                            transition: 'transform 0.2s ease',
-                                            zIndex: 10,
-                                        }}
-                                        onClick={(e) => handleCardClick(card, e)}
-                                    />
-                                );
-                            })}
+                    <div className="bita">
+                        <div className="card-container">
+                            <img
+                                key={'bita1'}
+                                src={back_card}
+                                alt={"back_card"}
+                                width={64}
+                                height={90}
+                                className="back_card1"
+                            />
+                            <img
+                                key={'bita2'}
+                                src={back_card}
+                                alt={"back_card"}
+                                width={64}
+                                height={90}
+                                className="back_card2"
+                            />
+                            <img
+                                key={'bita3'}
+                                src={back_card}
+                                alt={"back_card"}
+                                width={64}
+                                height={90}
+                                className="back_card3"
+                            />
                         </div>
+                    </div>
 
+                    <div className="table-card" ref={cardAnimationContainerRef}>
+                        {selectedCard && (
+                            <img
+                                src={getCardImagePath(selectedCard)}
+                                alt={selectedCard}
+                                className={`bita-card ${isAnimating ? 'animate' : ''}`}
+                                onAnimationEnd={() => {
+                                    setIsAnimating(false);
+                                    setSelectedCard(null);
+                                }}
+                            />
+                        )}
+                    </div>
+
+                    <div className="hand" ref={handRef}>
+                        {gameData && gameData.hand.map((card: string, index: number) => {
+                            const rotation = (index - middle) * angle;
+                            const position = (index - middle) * offset;
+
+                            return (
+                                <img
+                                    key={card}
+                                    src={getCardImagePath(card)}
+                                    alt={card}
+                                    style={{
+                                        left: `calc(50% + ${position}px)`,
+                                        transform: `rotate(${rotation}deg)`,
+                                        transition: 'transform 0.2s ease',
+                                        zIndex: 10,
+                                    }}
+                                    onClick={(e) => handleCardClick(card, e)}
+                                />
+                            );
+                        })}
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="play-footer">
+                <div className="play-footer-ava">
+                    <div className="footer-ava-roga">
+                        <div className="footer-ava-wp">
+                            <img src={GamePlay} alt="Gameplay Avatar"/>
+                        </div>
                     </div>
                 </div>
-
-                <div className="play-footer">
-                    <div className="play-footer-ava">
-                        <div className="footer-ava-roga">
-                            <div className="footer-ava-wp">
-                                <img src={GamePlay} alt="Gameplay Avatar"/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="play-footer-wrap">
-                        <div className="play-footer-block">
-                            <div className="play-footer-btn">Бито</div>
-                        </div>
+                <div className="play-footer-wrap">
+                    <div className="play-footer-block">
+                        <div className="play-footer-btn" onClick={handleBitaClick}>Бито</div>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
